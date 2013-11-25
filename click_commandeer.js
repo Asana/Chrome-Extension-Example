2a90a8ec60067809d85c0e73fc2b4b03c25cac01
Alex Davies <alexd@asana.com>
2013-11-25 11:18:52 -0800

    Intercept all clicks, and intercept when the A element isn’t the deepest element

--- click_commandeer.js
+++ click_commandeer.js
@@ -3,10 +3,9 @@ document.onclick = clickHandler;
 function clickHandler(e){
   if (!e) e = window.event;
 
-  if ((e.metaKey === true ||
-      e.ctrlKey === true || e.button == 1)
-      && $(e.target).prop("tagName") == "A"){
-    var link_url = $(e.target).prop("href") ;
+  var closestAnchor = $(e.target).closest("A");
+  if (closestAnchor.length === 1){
+    var link_url = closestAnchor.prop("href");
     if (link_url.indexOf("https://app.asana.com") != -1) {
       console.log("Asana chrome extension intercepting link ctrl-click")
       var fragment = link_url.substr("https://app.asana.com".length)

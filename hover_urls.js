a102371a3c71488f9bc49acceff6eb7780314cd4
Josh Smith <josh@asana.com>
2014-03-26 18:37:23 -0700

    Find and log asana urls on the page

--- hover_urls.js
+++ hover_urls.js
@@ -1,8 +1,13 @@
 hoverUrls();
 
 function hoverUrls() {
-//  var links = document.getElementsByTagName("a");
-//  links.filter(function(link) {
-//
-//  })
+  var links = document.getElementsByTagName("a");
+  var matching_links = [];
+  for (var i = 0; i < links.length; i++) {
+    var href = links[i].href;
+    if (href && (href.match(/.*\/\/app\.asana.com.*/) !== null)) {
+      matching_links.push(links[i]);
+    }
+  }
+  console.log(matching_links.map(function(a){ return a.href; }).join(","));
 }
